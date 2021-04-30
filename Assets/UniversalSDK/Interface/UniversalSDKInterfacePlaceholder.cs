8c367cf9c9079cb36f286ca52bb9d821895b5b36
james-chun <chc3484@gmail.com>
2021-04-30 18:07:33 +0900

    [update] interface 파라미터 추가

--- Assets/UniversalSDK/Interface/UniversalSDKInterfacePlaceholder.cs
+++ Assets/UniversalSDK/Interface/UniversalSDKInterfacePlaceholder.cs
@@ -3,7 +3,7 @@ namespace Universal.UniversalSDK
 {
     public class NativeInterface
     {
-        public static void SetupSDK(string identifier) { }
+        public static void SetupSDK(string identifier, string list) { }
         public static void Login(string identifier, LoginType loginType, AccountServiceType serviceType) { }
         public static void Logout(string identifier, LoginType loginType) { }
         public static void InAppPurchase(string identifier, string pid) { }
